a17cfef246b16e6c4c2e6e6e81cd2be1a9557285
williamreppelinn <133037747+williamreppelinn@users.noreply.github.com>
2023-10-27 13:14:09 +0200

    Update petitjeu.js

--- petitjeu.js
+++ petitjeu.js
@@ -1,5 +1,5 @@
-let pokemon1 = [["pikachu", 50, 10, 50, 0.3], ["salameche", 40, 15, 60, 0.2]];
-let pokemonTour = 1
+let pokemon1 = [["pikachu", 12, 6, 50, 0.7], ["salameche", 8, 4, 60, 0.5]];
+let pokemonTour = 0
 let mort = false
 
 function startMatch(){
@@ -14,11 +14,11 @@ function getRandom(){
 function isLucky(pokemon1, pokemonTour){
     let aleatoire = getRandom()
     if (aleatoire >= pokemon1[pokemonTour][4]){
-        attackPokemon(tour(pokemonTour), pokemon1)
+        attackPokemon(pokemonTour, pokemon1)
 
     }
     else{
-        console.log("ton pokemon ne peut attaquer, passe ton tour")
+        console.log("joueur numéro " + (pokemonTour + 1) + " ton pokemon ne peut attaquer, passe ton tour")
         isLucky(pokemon1,tour(pokemonTour) )
     }
 }
@@ -29,10 +29,10 @@ function attackPokemon(pokemonTour, pokemon1){
     if (degats > 0){
         pokemon1[1-pokemonTour][3] = pokemon1[1-pokemonTour][3] - degats
         if (pokemon1[1-pokemonTour][3] <=0){
-            console.log("il reste à l'ennemi 0 points de vie, vous lui avez faits " + degats + " de dégats")
+            console.log("joueur numéro " + tour(pokemonTour) +" il reste à l'ennemi 0 points de vie, vous lui avez faits " + degats + " de dégats")
         }
         else{
-            console.log("il reste à l'ennemi " + pokemon1[1-pokemonTour][3]+ " points de vie, vous lui avez faits " + degats + " de dégats")
+            console.log("joueur numéro " + (pokemonTour+1) + " il reste à l'ennemi " + pokemon1[1-pokemonTour][3]+ " points de vie, vous lui avez faits " + degats + " de dégats")
         }
         
         let pv = pokemon1[1-pokemonTour][3]
@@ -40,7 +40,7 @@ function attackPokemon(pokemonTour, pokemon1){
         return pv;
     }
     else{
-        console.log("vous n'avez pas fait assez de dégats")
+        console.log("joueur numéro " + (pokemonTour+1) + " vous n'avez pas fait assez de dégats")
         isLucky(pokemon1, tour(pokemonTour))
     }
     
@@ -60,10 +60,10 @@ function tour(pokemonTour){
 
 function death(pv, pokemonTour, pokemon1){
     if (pv <= 0){
-        console.log("le joueur numéro " + tour(pokemonTour) + " as perdu")
+        console.log("le joueur numéro " + (tour(pokemonTour) + 1) + " as perdu")
     }
     else{
-        isLucky(pokemon1, pokemonTour)
+        isLucky(pokemon1, tour(pokemonTour))
     }
 
 }
